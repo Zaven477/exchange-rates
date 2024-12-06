d59cc0a90bc18e380cea2e871cfbbcfdc573e052
Zaven477 <Papikyan86@bk.ru>
2024-12-06 12:27:52 +0400

    create exchangeRatesSlice

---
+++ src/store/exchangeRatesSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { TCurrency, TCurrentDateExchange } from "./types";
+
+const initialState: TCurrency &
+  TCurrentDateExchange & { loading: boolean; error: null | string } = {
+  exchangeCurrency: [],
+  currentDate: "",
+  loading: false,
+  error: null,
+};
+
+const exchangeRatesSlice = createSlice({
+  name: "rates",
+  initialState,
+  reducers: {
+    setExchangeCurrency: (state, { payload }) => {
+      state.exchangeCurrency.push(payload);
+    },
+    setCurrentDate: (state, { payload }) => {
+      state.currentDate = payload;
+    },
+    setLoading: (state, { payload }) => {
+      state.loading = payload;
+    },
+    setError: (state, { payload }) => {
+      state.error = payload;
+    },
+  },
+});
+
+export const {
+  setExchangeCurrency,
+  setCurrentDate,
+  setLoading,
+  setError,
+} = exchangeRatesSlice.actions;
+
+export default exchangeRatesSlice.reducer;
